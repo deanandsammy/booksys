1daaa8183d0bf1acf058edd5809dc822b727bea8
elegantspirit <hkissbigapple@163.com>
2018-03-27 20:22:40 +0800

    add private field

--- firstWeb/src/com/cm/servlet/DeleteServlet.java
+++ firstWeb/src/com/cm/servlet/DeleteServlet.java
@@ -1,5 +1,7 @@
 package com.cm.servlet;
 
+import com.cm.dao.BookDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DeleteServlet extends HttpServlet {
+    private BookDao bookDao = new BookDao();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
